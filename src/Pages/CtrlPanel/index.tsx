c0f490684605c5f0a8efe5459832991478b9826a
Matheus Fischer <matheusfischerr@icloud.com>
2020-05-20 01:37:54 -0300

    ctrl panel index.tsx

--- src/Pages/CtrlPanel/index.tsx
+++ src/Pages/CtrlPanel/index.tsx
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 import { Header, Grid } from './style';
 
 const CtrlPanel: React.FC = () => {
-    return (
+    return (    
         <>
             <Header>
                 <Link to="/control-panel">
